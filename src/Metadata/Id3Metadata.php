de507fe6ecd6014d317f2c3a182415a591b0bcca
Yemistikris <yemistikris@macbook-pro-de-yemistikris-1.home>
2015-12-23 14:54:31 +0100

    Add escape for commandline args

--- src/Metadata/Id3Metadata.php
+++ src/Metadata/Id3Metadata.php
@@ -3,8 +3,23 @@
 namespace Cpyree\Id3\Metadata;
 
 
-class Id3Metadata extends Id3MetadataBase implements Id3MetadataInterface
+class Id3Metadata extends Id3MetadataBase implements Id3MetadataInterface, \JsonSerializable
 {
 
 
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    function jsonSerialize()
+    {
+        $json = array();
+        foreach($this as $key => $value) {
+            $json[$key] = $value;
+        }
+        return $json;
+    }
 }
